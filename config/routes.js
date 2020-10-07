5fd610891d90ed95c32f80077509872e7f71f62e
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-10-08 02:48:28 +0300

    Improve request handler for serveral tables

--- config/routes.js
+++ config/routes.js
@@ -1,23 +1,20 @@
 const router = require("express").Router();
-const visitor = require("../controllers/Visitor.controller");
+const { findAll, findOne, create, update, destroy } = require("../controllers/Controller");
+// destroy == DELETE Request
 
-//
-// Table "Visitor"
-//
-
-// Return all or requested one data from Table "Visitor"
-router.get("/Visitor", visitor.findAll);
-// Return a single data row from "Visitor" with id
-router.get("/Visitor/:id", visitor.findOne);
-// Create new data into "Visitor"
-router.post("/Visitor", visitor.create);
-// Update data in "Visitor" found by query
-router.put("/Visitor", visitor.update);
-// Update data in "Visitor" by id
-router.put("/Visitor/:id", visitor.update);
-// Delete data from "Visitor" that found by query
-router.delete("/Visitor", visitor.delete);
-// Delete data from "Visitor" by id
-router.delete("/Visitor/:id", visitor.delete);
+// Return all or requested one data from <Table>
+router.get("/:table", findAll);
+// Return a single data row from <Table> with id
+router.get("/:table/:id", findOne);
+// Create new data into "<Table>
+router.post("/:table", create);
+// Update data in <Table> found by query
+router.put("/:table", update);
+// Update data in <Table> by id
+router.put("/:table/:id", update);
+// Delete data from <Table> that found by query
+router.delete("/:table", destroy);
+// Delete data from <Table> by id
+router.delete("/:table/:id", destroy);
 
 module.exports = router;
--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,3 +1,4 @@
+// Basic Output Style
 exports.logRequest = (...info) => {
   console.log(`\x1b[34m[${info[0]} REQUEST]\x1b[0m\x1b[35m[${getTime()}]\x1b[0m`, ...info.slice(1));
   function getTime() {
@@ -6,4 +7,67 @@ exports.logRequest = (...info) => {
   }
 };
 
+// Basic Output Style
 exports.logError = (err) => console.log(`\x1b[31m[Error]\x1b[0m ${err}`);
+
+//
+// REDIRECTION
+//
+const visitor = require("./Visitor.controller");
+const views = require("./Views.controller");
+const github = require("./Github.controller");
+
+function getTable(name) {
+  switch (name) {
+    case "Visitor":
+      return visitor;
+
+    case "Views":
+      return views;
+
+    case "Github":
+      return github;
+  }
+}
+
+// REDIRECT GET Request to the <Table>
+exports.findAll = (req, res) => {
+  let table = getTable(req.params.table);
+  delete req.params.table;
+
+  if (table) table.findAll(req, res);
+  else this.logError || res.status(404).send({ message: "Table not Found" });
+};
+
+// REDIRECT GET Request to the <Table>
+exports.findOne = (req, res) => {
+  let table = getTable(req.params.table);
+  delete req.params.table;
+
+  if (table) table.findOne(req, res);
+  else this.logError || res.status(404).send({ message: "Table not Found" });
+};
+
+exports.create = (req, res) => {
+  let table = getTable(req.params.table);
+  delete req.params.table;
+
+  if (table) table.create(req, res);
+  else this.logError || res.status(404).send({ message: "Table not Found" });
+};
+
+exports.update = (req, res) => {
+  let table = getTable(req.params.table);
+  delete req.params.table;
+
+  if (table) table.update(req, res);
+  else this.logError || res.status(404).send({ message: "Table not Found" });
+};
+
+exports.destroy = (req, res) => {
+  let table = getTable(req.params.table);
+  delete req.params.table;
+
+  if (table) table.destroy(req, res);
+  else this.logError || res.status(404).send({ message: "Table not Found" });
+};
