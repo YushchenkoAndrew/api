28fcaece8b36c56578b399282021337a25c0f8fb
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-10-09 01:25:39 +0300

    Correct CRUD Controller Structure

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -13,9 +13,9 @@ exports.logError = (err) => console.log(`\x1b[31m[Error]\x1b[0m ${err}`);
 //
 // REDIRECTION
 //
-const visitor = require("./Visitor.controller");
-const views = require("./Views.controller");
-const github = require("./Github.controller");
+const visitor = require("../services/Visitor.service");
+const views = require("../services/Views.service");
+const github = require("../services/Github.service");
 
 function getTable(name) {
   switch (name) {
@@ -33,28 +33,68 @@ function getTable(name) {
 // REDIRECT GET Request to the <Table>
 exports.findAll = (req, res) => {
   let table = getTable(req.params.table);
-  delete req.params.table;
+  let query = Object.keys(req.query).length ? req.query : null;
+  this.logRequest("GET", `TABLE = '${table}' QUERY =`, query);
 
-  if (table) table.findAll(req, res);
+  if (table)
+    table.find(
+      query,
+      // Callback
+      (data) => res.send(data),
+      // Error
+      (err) => this.logError(err) || res.status(500).send({ message: err.message })
+    );
   else this.logError("Table not Found") || res.status(404).send({ message: "Table not Found" });
 };
 
 // REDIRECT GET Request to the <Table>
 exports.findOne = (req, res) => {
   let table = getTable(req.params.table);
-  delete req.params.table;
+  this.logRequest("GET", `TABLE = '${table}' ID =`, req.params.id);
 
-  if (table) table.findOne(req, res);
+  // Bad Request handler
+  if (req.params.id === undefined) {
+    this.logError("Invalid request message parameters");
+    res.status(400).send({ message: "Invalid request message parameters" });
+    return;
+  }
+
+  if (table)
+    table.find(
+      // Query
+      { id: req.params.id },
+      // Callback
+      (data) => res.send(data),
+      // Error
+      (err) => this.logError(err) || res.status(500).send({ message: err.message })
+    );
   else this.logError("Table not Found") || res.status(404).send({ message: "Table not Found" });
 };
 
 // REDIRECT POST Request to the <Table>
 exports.create = (req, res) => {
   let table = getTable(req.params.table);
-  delete req.params.table;
+  // delete req.params.table;
+  this.logRequest("POST", "TABLE = 'Views' DATA =", req.body);
 
-  if (table) table.create(req, res);
-  else this.logError("Table not Found") || res.status(404).send({ message: "Table not Found" });
+  if (table) {
+    let data = new table.Data(req.body);
+
+    // Bad Request handler
+    if (data.check()) {
+      this.logError("Invalid request message parameters");
+      res.status(400).send({ message: "Invalid request message parameters" });
+      return;
+    }
+
+    table.create(
+      data,
+      // Callback
+      (data) => res.status(201).send(data),
+      // Error
+      (err) => logError(err) || res.status(500).send({ message: err.message })
+    );
+  } else this.logError("Table not Found") || res.status(404).send({ message: "Table not Found" });
 };
 
 // REDIRECT PUT Request to the <Table>
@@ -62,7 +102,26 @@ exports.update = (req, res) => {
   let table = getTable(req.params.table);
   delete req.params.table;
 
-  if (table) table.update(req, res);
+  this.logRequest("PUT", "TABLE = 'Views' CONDITION =", { ...req.query, ...req.params }, "DATA = ", req.body);
+
+  // Check if parameters and updated data
+  if (!Object.keys(req.body).length || (!Object.keys(req.query).length && !Object.keys(req.params).length)) {
+    this.logError("Invalid request message parameters");
+    res.status(400).send({ message: "Invalid request message parameters" });
+    return;
+  }
+
+  if (table)
+    table.update(
+      // Data
+      req.body,
+      // Query
+      { ...req.query, ...req.params },
+      // Callback
+      ([data]) => (data ? res.sendStatus(204) : res.sendStatus(304)),
+      // Error
+      (err) => this.logError(err) || res.status(500).send({ message: err.message })
+    );
   else this.logError("Table not Found") || res.status(404).send({ message: "Table not Found" });
 };
 
@@ -70,7 +129,23 @@ exports.update = (req, res) => {
 exports.destroy = (req, res) => {
   let table = getTable(req.params.table);
   delete req.params.table;
+  this.logRequest("DELETE", "TABLE = 'Views' CONDITION =", { ...req.query, ...req.params });
 
-  if (table) table.destroy(req, res);
+  // Bad Request handler
+  if (!Object.keys(req.query).length && !Object.keys(req.params).length) {
+    this.logError("Invalid request message parameters");
+    res.status(400).send({ message: "Invalid request message parameters" });
+    return;
+  }
+
+  if (table)
+    table.destroy(
+      // Query
+      { ...req.query, ...req.params },
+      // Callback
+      (data) => (data ? res.sendStatus(204) : res.sendStatus(304)),
+      // Error
+      (err) => this.logError(err) || res.status(500).send({ message: err })
+    );
   else this.logError("Table not Found") || res.status(404).send({ message: "Table not Found" });
 };
