a9a376c6113ad2ecf26199cdf752cddf431e1e82
Yushchenko Andrew <Andrew10122000@gmail.com>
2021-08-26 02:18:12 +0300

    Add swagger docs

--- routes/info.go
+++ routes/info.go
@@ -10,14 +10,22 @@ func (*Routes) info(rg *gin.RouterGroup) {
 	route := rg.Group("/info")
 	cInfo := c.InfoController{}
 
+	// @Success 200 {string} string	"ok"
+	// @failure 400 {string} string	"error"
+	// @response default {string} string	"other error"
+	// @Header 200 {string} Location "/entity/1"
+	// @Header 200,400,default {string} Token "token"
+	// @Header all {string} Token2 "token2"
 	route.POST("", cInfo.CreateOne)
+
 	route.POST("/list", cInfo.CreateAll)
 
 	route.GET("", cInfo.ReadAll)
 	route.GET("/:id", cInfo.ReadOne)
 
-	route.PUT("/:id", cInfo.UpdateOne)
 	route.PUT("", cInfo.UpdateAll)
+	route.PUT("/:id", cInfo.UpdateOne)
 
-	route.DELETE("", cInfo.Delete)
+	route.DELETE("", cInfo.DeleteAll)
+	route.DELETE("/:id", cInfo.DeleteOne)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -2,16 +2,49 @@ package routes
 
 import (
 	"api/config"
+	info "api/routes/info"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	_ "api/docs"
 
 	"github.com/gin-gonic/gin"
 )
 
-type Routes struct{}
+type SubRoutes struct {
+	info info.Routes
+}
+type Routes struct {
+	sub SubRoutes
+}
 
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /api
+
+// @Router /info [get]
 func (r *Routes) Init(rg *gin.Engine) {
 	route := rg.Group(config.ENV.BasePath)
 
 	r.index(route)
 	r.info(route)
 	r.world(route)
+
+	// Init SubRoutes
+	r.sub.info.Init(route)
+
+	// url := ginSwagger.URL("http://localhost:31337/api/swagger/doc.json")
+	route.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
