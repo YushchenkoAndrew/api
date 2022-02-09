c797e3cfeb0f278bd5ca85abec0d444a73e7be8d
Andrew Y <Andrew10122000@gmail.com>
2022-02-09 19:15:27 +0200

    Move subscription logic to another controller

--- controllers/k3s.go
+++ controllers/k3s.go
@@ -1,19 +1,7 @@
 package controllers
 
 import (
-	"api/config"
-	"api/db"
 	"api/interfaces"
-	"api/models"
-	"bytes"
-	"context"
-	"crypto/md5"
-	"encoding/hex"
-	"encoding/json"
-	"fmt"
-	"math/rand"
-	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -35,7 +23,7 @@ func NewK3sController() interfaces.K3s {
 // @Produce application/xml
 // @Security BearerAuth
 // @Param id path int true "Project primaray id"
-// @Param model body models.ReqLink true "Link info"
+// @Param model body models.SubscribeDto true "Small info about subscription for k3s"
 // @Success 201 {object} models.Success{result=[]models.Link}
 // @failure 400 {object} models.Error
 // @failure 401 {object} models.Error
@@ -44,52 +32,52 @@ func NewK3sController() interfaces.K3s {
 // @failure 500 {object} models.Error
 // @Router /subscribe [post]
 func (*k3sController) Subscribe(c *gin.Context) {
-	var err error
-	var body []byte
+	// var err error
+	// var body []byte
 
-	hasher := md5.New()
-	hasher.Write([]byte(strconv.Itoa(rand.Intn(1000000) + 5000)))
-	token := hex.EncodeToString(hasher.Sum(nil))
+	// hasher := md5.New()
+	// hasher.Write([]byte(strconv.Itoa(rand.Intn(1000000) + 5000)))
+	// token := hex.EncodeToString(hasher.Sum(nil))
 
-	if body, err = json.Marshal(models.CronCreateDto{
-		CronTime: SUBSCRIBE_TIME,
-		URL:      fmt.Sprintf("%s/k3s/pods/metrics", config.ENV.URL),
-		Method:   "post",
-		ApiKey:   token,
-	}); err != nil {
-		fmt.Println("Ohh noo; Anyway")
-		return
-	}
+	// if body, err = json.Marshal(models.CronCreateDto{
+	// 	CronTime: SUBSCRIBE_TIME,
+	// 	URL:      fmt.Sprintf("%s/k3s/pods/metrics", config.ENV.URL),
+	// 	Method:   "post",
+	// 	Key:      token,
+	// }); err != nil {
+	// 	fmt.Println("Ohh noo; Anyway")
+	// 	return
+	// }
 
-	hasher = md5.New()
-	var salt = strconv.Itoa(rand.Intn(1000000))
-	hasher.Write([]byte(salt + config.ENV.BotKey))
+	// hasher = md5.New()
+	// var salt = strconv.Itoa(rand.Intn(1000000))
+	// hasher.Write([]byte(salt + config.ENV.BotKey))
 
-	var req *http.Request
-	if req, err = http.NewRequest("POST", config.ENV.BotUrl+"/cron?key="+hex.EncodeToString(hasher.Sum(nil)), bytes.NewBuffer(body)); err != nil {
-		fmt.Println("Ohh noo; Anyway")
-		return
-	}
+	// var req *http.Request
+	// if req, err = http.NewRequest("POST", config.ENV.BotUrl+"/cron?key="+hex.EncodeToString(hasher.Sum(nil)), bytes.NewBuffer(body)); err != nil {
+	// 	fmt.Println("Ohh noo; Anyway")
+	// 	return
+	// }
 
-	req.Header.Set("X-Custom-Header", salt)
-	req.Header.Set("Content-Type", "application/json")
+	// req.Header.Set("X-Custom-Header", salt)
+	// req.Header.Set("Content-Type", "application/json")
 
-	var res *http.Response
-	client := &http.Client{}
-	res, err = client.Do(req)
-	if err != nil {
-		fmt.Println("Ohh noo; Anyway")
-		return
-	}
+	// var res *http.Response
+	// client := &http.Client{}
+	// res, err = client.Do(req)
+	// if err != nil {
+	// 	fmt.Println("Ohh noo; Anyway")
+	// 	return
+	// }
 
-	// TODO: Save Request response into db
-	defer res.Body.Close()
+	// // TODO: Save Request response into db
+	// defer res.Body.Close()
 
-	hasher = md5.New()
-	hasher.Write([]byte(token))
+	// hasher = md5.New()
+	// hasher.Write([]byte(token))
 
-	ctx := context.Background()
-	db.Redis.Set(ctx, "TOKEN:"+hex.EncodeToString(hasher.Sum(nil)), "OK", 0)
+	// ctx := context.Background()
+	// db.Redis.Set(ctx, "TOKEN:"+hex.EncodeToString(hasher.Sum(nil)), "OK", 0)
 }
 
 // @Tags K3s
---
+++ controllers/subscription.go
@@ -0,0 +1,159 @@
+package controllers
+
+import (
+	"api/config"
+	"api/db"
+	"api/helper"
+	"api/interfaces"
+	"api/logs"
+	"api/models"
+	"bytes"
+	"context"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type subscriptionController struct{}
+
+func NewSubscriptionController() interfaces.Default {
+	return &subscriptionController{}
+}
+
+func (*subscriptionController) CreateAll(c *gin.Context) {}
+
+// @Tags Subscription
+// @Summary Create Subscription to run operation
+// @Accept json
+// @Produce application/json
+// @Produce application/xml
+// @Security BearerAuth
+// @Param model body models.SubscribeDto true "Small info about subscription for k3s"
+// @Success 201 {object} models.Success{result=[]models.DefultRes}
+// @failure 400 {object} models.Error
+// @failure 401 {object} models.Error
+// @failure 422 {object} models.Error
+// @failure 429 {object} models.Error
+// @failure 500 {object} models.Error
+// @Router /subscription [post]
+func (*subscriptionController) CreateOne(c *gin.Context) {
+	var err error
+	var body models.SubscribeDto
+	if err = c.ShouldBind(&body); err != nil || body.CronTime == "" {
+		helper.ErrHandler(c, http.StatusBadRequest, "Incorrect body params")
+		return
+	}
+
+	handler, ok := config.GetOperation(body.Operation)
+	if !ok {
+		helper.ErrHandler(c, http.StatusNotFound, fmt.Sprintf("Operation '%s' not founded", body.Operation))
+		return
+	}
+
+	hasher := md5.New()
+	hasher.Write([]byte(strconv.Itoa(rand.Intn(1000000) + 5000)))
+	token := hex.EncodeToString(hasher.Sum(nil))
+
+	var reqBody []byte
+	if reqBody, err = json.Marshal(models.CronCreateDto{
+		CronTime: body.CronTime,
+		URL:      config.ENV.URL + handler.Path,
+		Method:   handler.Method,
+		Key:      token,
+	}); err != nil {
+		fmt.Println("Ohh noo; Anyway")
+		return
+	}
+
+	hasher = md5.New()
+	salt := strconv.Itoa(rand.Intn(1000000) + 5000)
+	hasher.Write([]byte(salt + config.ENV.BotKey))
+
+	var req *http.Request
+	if req, err = http.NewRequest("POST", config.ENV.BotUrl+"/cron?key="+hex.EncodeToString(hasher.Sum(nil)), bytes.NewBuffer(reqBody)); err != nil {
+		fmt.Println("Ohh noo; Anyway")
+		return
+	}
+
+	req.Header.Set("X-Custom-Header", salt)
+	req.Header.Set("Content-Type", "application/json")
+
+	var res *http.Response
+	client := &http.Client{}
+	res, err = client.Do(req)
+	if err != nil {
+		helper.ErrHandler(c, http.StatusInternalServerError, "Server side error: Something went wrong response")
+		return
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusCreated {
+		helper.ErrHandler(c, res.StatusCode, "Bot request error")
+		return
+	}
+
+	var cron models.CronRes
+	if err = json.NewDecoder(res.Body).Decode(&cron); err != nil {
+		helper.ErrHandler(c, http.StatusInternalServerError, "Server side error: Something went wrong response")
+		return
+	}
+
+	result := db.DB.Create(&models.Subscription{
+		CronID:    cron.ID,
+		CronTime:  cron.Exec.CronTime,
+		Operation: body.Operation,
+	})
+
+	if result.Error != nil || result.RowsAffected == 0 {
+		helper.ErrHandler(c, http.StatusInternalServerError, "Something unexpected happend")
+		go logs.DefaultLog("/controllers/subscription.go", result.Error)
+		return
+	}
+
+	go func() {
+		hasher = md5.New()
+		hasher.Write([]byte(token))
+
+		ctx := context.Background()
+		db.Redis.Set(ctx, "TOKEN:"+hex.EncodeToString(hasher.Sum(nil)), "OK", 0)
+	}()
+
+	helper.ResHandler(c, http.StatusOK, models.DefultRes{
+		Status:  "OK",
+		Message: "Success",
+		Result:  []string{},
+	})
+}
+
+func (*subscriptionController) ReadOne(c *gin.Context)   {}
+func (*subscriptionController) ReadAll(c *gin.Context)   {}
+func (*subscriptionController) UpdateOne(c *gin.Context) {}
+func (*subscriptionController) UpdateAll(c *gin.Context) {}
+
+// @Tags Subscription
+// @Summary Create
+// @Accept json
+// @Produce application/json
+// @Produce application/xml
+// @Security BearerAuth
+// @Param id path int true "Project primaray id"
+// @Param model body models.ReqLink true "Link info"
+// @Success 201 {object} models.Success{result=[]models.Link}
+// @failure 400 {object} models.Error
+// @failure 401 {object} models.Error
+// @failure 422 {object} models.Error
+// @failure 429 {object} models.Error
+// @failure 500 {object} models.Error
+// @Router /link/{id} [post]
+func (*subscriptionController) DeleteOne(c *gin.Context) {
+	// TODO: Finish the thing above !!!
+}
+
+func (*subscriptionController) DeleteAll(c *gin.Context) {}
